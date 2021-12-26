7239a776fa6d0a5956d33d5f8b0741d989a20dea
StefanoIncardone <stefanoincardone@gmail.com>
2021-12-26 19:27:53 +0100

    renamed "Components" test folder to "components"

---
+++ java/CPU/src/test/java/com/stefano/components/ALU/adders/multipleBitsInput/rippleCarryAdders/twoBits/RCA_2bitsTest.java
@@ -0,0 +1,44 @@
+package com.stefano.components.ALU.adders.multipleBitsInput.rippleCarryAdders.twoBits;
+
+import static org.junit.Assert.*;
+
+import com.stefano.components.ALU.adders.multipleBitsInput.rippleCarry.twoBits.TwoBitsRCA;
+
+import org.junit.Test;
+
+public class RCA_2bitsTest
+{
+	private final TwoBitsRCA rca_2 = new TwoBitsRCA();
+
+	@Test
+	public void twoBitsRCAShouldNotSetCarryIfSumIsLessThanThree()
+	{
+		assertEquals( 0, rca_2.out( 0, 0, 0, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 0, 0, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 1, 0, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 0, 1, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 1, 0, 0, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 0, 1, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 1, 1, 0, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 1, 0, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 1, 0, 0, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 0, rca_2.out( 0, 1, 1, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+	}
+
+	@Test
+	public void twoBitsRCAShouldSetCarryIfSumIsMoreThanThree()
+	{
+		assertEquals( 1, rca_2.out( 1, 0, 1, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 1, rca_2.out( 0, 1, 1, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 1, rca_2.out( 1, 1, 0, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 1, rca_2.out( 1, 0, 1, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 1, rca_2.out( 1, 1, 1, 0 )[ TwoBitsRCA.CARRY_OUT ] );
+		assertEquals( 1, rca_2.out( 1, 1, 1, 1 )[ TwoBitsRCA.CARRY_OUT ] );
+	}
+
+	@Test
+	public void twoBitsRCAShouldOutputZeroWhenInputIsZero()
+	{
+		assertArrayEquals( new byte[]{ 0, 0, 0 }, rca_2.out( 0, 0, 0, 0 ) );
+	}
+}
